147950f42acf7956913769da40337a4bfe0e8f50
TrulsThur <trulsthur@outlook.com>
2024-11-25 14:13:09 +0100

    code

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿string row1 = "...";
+string row2 = "...";
+string row3 = "...";
+char player =  'x';
+
+bool run = true;
+while (run)
+{
+    
+    Console.WriteLine(row1);
+    Console.WriteLine(row2);
+    Console.WriteLine(row3);
+    
+    Console.WriteLine("enter row or q for quit");
+    string input = Console.ReadLine();
+    
+    if(input!="q") {
+        Console.WriteLine("enter col");
+        string inputcol = Console.ReadLine();
+        int colno = int.Parse(inputcol);
+        colno = colno - 1;
+        if (input == "1")
+        {
+            char[] chars = row1.ToCharArray();
+            chars[colno] = player;
+            row1 = new string(chars);
+        }
+
+        if (input == "2")
+        {
+            char[] chars = row2.ToCharArray();
+            chars[colno] = player;
+            row2 = new string(chars);
+        }
+
+        if (input == "3")
+        {
+            char[] chars = row3.ToCharArray();
+            chars[colno] = player;
+            row3 = new string(chars);
+        }
+
+        if (player == 'x')
+        {
+            player = 'o';
+        }
+        else { player = 'x'; }
+    }
+    else
+    {
+        run = false;
+    }
+}
+
+
+
---
+++ ConsoleApp1/ConsoleApp1/Stored.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Stored
+    {
+        // See https://aka.ms/new-console-template for more information
+
+
+//        var name = "Fredrik";
+//        string lastName = "Andersson";
+//        int age = 34;
+//        float height = 177.5f;
+//        char lastNameInitial = 'A';
+
+
+//        Console.WriteLine("Hello, World!");
+//Console.WriteLine("Have you eaten breakfast today?");
+//var anwser = Console.ReadLine();
+//        Console.WriteLine(anwser);
+
+
+//var numberofBillGates = "teddy";
+//        Console.WriteLine(numberofBillGates);
+//Console.WriteLine(numberofBillGates);
+
+//long jeffBezoNetworth = 196000000000;
+
+//        var CharChar = "c";
+
+//        //Create Read Update Delete
+//        //CRUD
+//        //Create
+//        string petFish = "puffer fish";
+
+
+//        //Concatenation
+//        petFish = "porcupine " + petFish;
+
+//Console.WriteLine(petFish);
+
+
+//for(var i = 1; i <= 10; i++) {
+
+
+//    Console.WriteLine(i);
+
+    }
+}
